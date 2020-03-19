81fa3d37b4b2f89d68de7f39dca18892027a01a3
parkyes90 <parkyes90@gmail.com>
2020-03-19 23:00:31 +0900

    test($ProjectFileDir): Add Tests
    Index View Test
    Load Test

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,6 +1,17 @@
+import json
 import unittest
+from services.app import app
 
 
 class TestIndex(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = app.test_client()
+
     def test_index_view(self):
-        pass
+        response = self.client.get("/")
+        body = json.loads(response.data.decode("utf-8"))
+        self.assertEqual(body, {})
+
+
+if __name__ == "__main__":
+    unittest.main()
